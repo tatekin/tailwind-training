63359f653f210efd743f6dacbd378d40e44e1613
tatekin <yuukisasaki42@icloud.com>
2021-12-25 10:31:45 +0900

    create react app with ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div>
+    <h1 className='text-center text-3xl font-bold underline'>
+      HELLO WORLD!
+    </h1>
+  </div>
+)
+
+export default App;
